49c161ff6febda1b01a8a4b0701b40ccc62490a9
tommsonn <thomastes433@gmail.com>
2026-06-09 22:34:59 +0300

    Initialize Governance Service

---
+++ governance-policy-system/governance-service/src/test/java/com/governance/governance_service/GovernanceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.governance.governance_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GovernanceServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
