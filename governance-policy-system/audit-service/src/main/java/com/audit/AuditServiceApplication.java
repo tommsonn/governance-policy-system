bed8876f59c1fb4ce73956885a1ae393be345722
tommsonn <thomastes433@gmail.com>
2026-07-25 12:53:41 +0300

    add dependency and configuration for eureka server

--- governance-policy-system/audit-service/src/main/java/com/audit/AuditServiceApplication.java
+++ governance-policy-system/audit-service/src/main/java/com/audit/AuditServiceApplication.java
@@ -2,12 +2,12 @@ package com.audit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class AuditServiceApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(AuditServiceApplication.class, args);
 	}
-
 }
